046eb247963f38ca0017c6df13ed3d7142dce6c4
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-02-21 20:22:35 +0100

    Movement and look controls added.

---
+++ ProjectGuard/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+		private Vector3 myPos;
+		// Use this for initialization
+		void Start ()
+		{
+				myPos = this.transform.position;
+				this.transform.rotation = Quaternion.Euler (70, 0, 0);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Transform t = GameObject.FindGameObjectWithTag ("Player").transform;
+				myPos = new Vector3 (t.position.x, t.position.y + 6, t.position.z - 2);
+				this.transform.position = myPos;
+		}
+}
---
+++ ProjectGuard/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLook : MonoBehaviour
+{
+		public GameObject player;
+		private Vector3 mousePos;
+		private int cameraDistance;
+		private Vector3 worldPos;
+	
+		void Start ()
+		{
+				cameraDistance = (int)(camera.transform.position.y - player.transform.position.y);
+		}
+	
+		void FixedUpdate ()
+		{ 
+				mousePos = Input.mousePosition;
+				worldPos = camera.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, cameraDistance));
+			
+				player.transform.LookAt (worldPos);
+				player.transform.rotation = Quaternion.Euler (new Vector3 (0, player.transform.rotation.eulerAngles.y, 0));
+		}	
+}
