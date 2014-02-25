4ebf4c98bd2b13de5f78304d873206fe63c81bef
Ensutee <magnus_s_n@hotmail.com>
2014-02-25 06:54:34 +0100

    Added animations (Idle + Rebound) and also added a vidual effect upon rebound through a Spark prefab

--- ProjectGuard/Assets/Scripts/WeaponStats.cs
+++ ProjectGuard/Assets/Scripts/WeaponStats.cs
@@ -6,32 +6,46 @@ public class WeaponStats : MonoBehaviour {
 	public bool doesDamage;
 	public float damage;
 	public List<GameObject> targetsHit;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		doesDamage = false;
 		targetsHit = new List<GameObject>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButton(0)) {
+			anim.SetBool("Attack", true);
+		}
+		if (anim.GetNextAnimatorStateInfo(0).IsName("Idle")) {
+			anim.SetBool("Attack", false);
+			anim.SetBool("Rebound", false);
+		}
 	}
 
-	void OnTriggerEnter (Collider other) {
-		if (other.GetComponent<MonsterHealth>() != null && !targetsHit.Contains(other.gameObject) && doesDamage) {
-			print ("HIT!: " +other.name);
-			other.GetComponent<MonsterHealth>().health -= damage;
-			targetsHit.Add(other.gameObject);
+	void OnCollisionEnter (Collision col) {
+		GameObject hit = col.collider.gameObject;
+		if (hit.GetComponent<MonsterHealth>() != null && !targetsHit.Contains(hit) && doesDamage) {
+			print ("HIT!: " +hit.name);
+			hit.GetComponent<MonsterHealth>().health -= damage;
+			targetsHit.Add(hit);
+		} else if (hit.GetComponent<MonsterHealth>() == null && hit.tag != "Player" && anim.GetBool("Attack")) {
+			print ("REBOUND!");
+			GameObject sparks = (GameObject)Instantiate(Resources.Load("Prefabs/Sparks"), col.contacts[0].point, Quaternion.LookRotation(col.contacts[0].normal));
+			anim.SetBool("Attack", false);
+			anim.SetBool("Rebound", true);
+			NoDamage();
 		}
 	}
 
 	//called form animation
-	void Attacking () {
+	void Damage () {
 		doesDamage = true;
 	}
-
-	void StopAttacking () {
+	void NoDamage () {
 		doesDamage = false;
 	}
 
