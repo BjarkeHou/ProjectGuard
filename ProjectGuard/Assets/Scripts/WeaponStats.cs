fb92d3dee59d3c9765c32a4033a7fa941d6572fd
Ensutee <magnus_s_n@hotmail.com>
2014-02-25 01:25:38 +0100

    Added scripts to handle monster health and weapon stats

---
+++ ProjectGuard/Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WeaponStats : MonoBehaviour {
+
+	public bool doesDamage;
+	public float damage;
+	public List<GameObject> targetsHit;
+
+	// Use this for initialization
+	void Start () {
+		doesDamage = false;
+		targetsHit = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.GetComponent<MonsterHealth>() != null && !targetsHit.Contains(other.gameObject) && doesDamage) {
+			print ("HIT!: " +other.name);
+			other.GetComponent<MonsterHealth>().health -= damage;
+			targetsHit.Add(other.gameObject);
+		}
+	}
+
+	//called form animation
+	void Attacking () {
+		doesDamage = true;
+	}
+
+	void StopAttacking () {
+		doesDamage = false;
+	}
+
+	//called from animation
+	void ClearHits () {
+		targetsHit.Clear();
+	}
+}
