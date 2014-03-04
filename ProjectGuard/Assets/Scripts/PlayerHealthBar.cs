627c8cbb54648fbe72581193ffcc8f25f922d59e
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-03-04 15:14:37 +0100

    HealthController script (Skulle gerne kunne bruges generelt på alle characters der skal have liv) PlayerHealthBar omstrukturererererererereret til HealthController PlayerHealth og MonsterHealth fjernet PlayerController: Tilføjet en mulighed for at gøre så vores player ikke kan flytte sig. Kan slåes til og fra.

---
+++ ProjectGuard/Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+		private float healthBarLength;
+
+		// Use this for initialization
+		void Start ()
+		{
+				healthBarLength = Screen.width / 2;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnGUI ()
+		{
+//				GUI.backgroundColor = Color.red;
+				int curHealth = this.GetComponent<HealthController> ().getCurrentHealth ();
+				int maxHealth = this.GetComponent<HealthController> ().getMaxHealth ();
+				GUI.color = Color.red;
+				GUI.Box (new Rect (10, 10, healthBarLength, 20), curHealth + " / " + maxHealth);
+				
+		}
+}
