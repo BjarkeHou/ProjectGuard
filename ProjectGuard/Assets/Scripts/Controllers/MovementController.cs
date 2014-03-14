eb21a207672b8c08d33e85ded5b3cebedb851bef
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-03-14 20:10:20 +0100

    Lorte fucking merge.. Sourcetree er ikke altid the shit!!

--- ProjectGuard/Assets/Scripts/Controllers/MovementController.cs
+++ ProjectGuard/Assets/Scripts/Controllers/MovementController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MovementController : MonoBehaviour
 {
+	private CharacterController charCont;
 	private PlayerLook pLook;
 
 	public float normalMovementSpeed = 6;
@@ -12,14 +13,14 @@ public class MovementController : MonoBehaviour
 	public float dodgeDistance;
 	public AnimationCurve curve;
 	public float lerpSpeed;
-		
+	
 	private float movementSpeed;
 
 	private Vector3 moveDirection;
 	private bool playerCanMove = true;
 	private bool playerCanDodge = true;
 	private float progress;
-		
+	
 	private float dodgeDelayCounter = 0;
 	private float dodgedDistance = 0;
 	private bool inDodgeMove = false;
@@ -27,6 +28,7 @@ public class MovementController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		charCont = GetComponent<CharacterController> ();
 		pLook = this.GetComponent<PlayerLook> ();
 		movementSpeed = normalMovementSpeed;
 	}
@@ -34,21 +36,19 @@ public class MovementController : MonoBehaviour
 	public void MoveCharacter (float v, float h, bool shift)
 	{
 		if (inDodgeMove)
-			return;
-				
+			return;				
 		moveDirection = Vector3.zero;
-		;
-		
+	
 		if (v < 0) 
 			moveDirection += Vector3.back;
 		else if (v > 0)
 			moveDirection += Vector3.forward;
-		
+	
 		if (h < 0)
 			moveDirection += Vector3.left;
 		else if (h > 0)
 			moveDirection += Vector3.right;
-		
+	
 		// Update dodge delay timer
 		if (dodgeDelayCounter > 0) {
 			dodgeDelayCounter -= Time.deltaTime * 1000;
@@ -56,7 +56,7 @@ public class MovementController : MonoBehaviour
 		if (dodgeDelayCounter <= 0) {
 			dodgeDelayCounter = -1;
 		}
-				
+
 		// Test if player is trying to dodge
 		if (playerCanDodge && shift && playerCanMove && !inDodgeMove && dodgeDelayCounter == -1) {
 			Vector3 sPoint = this.transform.position;
@@ -70,64 +70,64 @@ public class MovementController : MonoBehaviour
 			}
 			StartCoroutine (PerformDodge (sPoint, dodgeDirection, dodgeDist));
 		} else if (!inDodgeMove) {
-			this.transform.position += (moveDirection * Time.deltaTime * movementSpeed);
+			charCont.Move (moveDirection * Time.deltaTime * movementSpeed);
 		}
 	}
-		
+	
 	private IEnumerator PerformDodge (Vector3 startPoint, Vector3 dodgeDir, float dodgeDist)
 	{
 		inDodgeMove = true;
 		pLook.LockPlayerOnDirection (dodgeDir);
 		dodgeDelayCounter = dodgeDelay;
-			
+
 		while (progress < 1.0f) {
 			progress = Mathf.InverseLerp (0, dodgeDist, dodgedDistance);
 			dodgedDistance += curve.Evaluate (progress) * dodgeSpeed * Time.deltaTime;	
 			Vector3 desiredPos = startPoint + dodgeDir.normalized * dodgedDistance;
-			this.transform.position = desiredPos; //Vector3.Lerp (this.transform.position, desiredPos, Time.deltaTime * lerpSpeed); 
+			charCont.Move (desiredPos - transform.position);
 			if (!pLook.playerCanRotate && progress > 0.8f)
 				pLook.playerCanRotate = true;
 			yield return null;
 		}
-			
+		
 		progress = 0.0f;
-		dodgedDistance = 0;	
+		dodgedDistance = 0;
 		inDodgeMove = false;
 		pLook.playerCanRotate = true;
+		
+		Debug.Log ("Dodge performed!!");
+	}
+/*
+	private IEnumerator PerformDodge (Vector3 startPoint, Vector3 dodgeDir, float dodgeDist)
+	{
+		inDodgeMove = true;
+
+		dodgeDelayCounter = dodgeDelay;
+	
+		while (progress < 1.0f) {
+			progress = Mathf.InverseLerp (0, dodgeDist, (startPoint - this.transform.position).magnitude);
+			Vector3 desiredPos = this.transform.position + dodgeDir.normalized * curve.Evaluate (progress) * Time.deltaTime * dodgeSpeed;
+			this.transform.position = Vector3.Lerp (this.transform.position, desiredPos, Time.deltaTime * lerpSpeed); 
+			yield return null;
+		}
+			
+		progress = 0.0f;
+		inDodgeMove = false;
 			
 		Debug.Log ("Dodge performed!!");
 	}
-	/*
-		private IEnumerator PerformDodge (Vector3 startPoint, Vector3 dodgeDir, float dodgeDist)
-		{
-				inDodgeMove = true;
-
-				dodgeDelayCounter = dodgeDelay;
-		
-				while (progress < 1.0f) {
-						progress = Mathf.InverseLerp (0, dodgeDist, (startPoint - this.transform.position).magnitude);
-						Vector3 desiredPos = this.transform.position + dodgeDir.normalized * curve.Evaluate (progress) * Time.deltaTime * dodgeSpeed;
-						this.transform.position = Vector3.Lerp (this.transform.position, desiredPos, Time.deltaTime * lerpSpeed); 
-						yield return null;
-				}
-				
-				progress = 0.0f;
-				inDodgeMove = false;
-				
-				Debug.Log ("Dodge performed!!");
-		}
-	*/	
+*/	
 	public void SetCanMove (bool value)
 	{
 		playerCanMove = value;
-				
+			
 		if (value) {
 			movementSpeed = normalMovementSpeed;
 		} else if (!value) {
 			movementSpeed = attackingMovementSpeed;
 		}
 	}
-		
+	
 	public void SetCanDodge (bool value)
 	{
 		playerCanDodge = value;
--- ProjectGuard/Assets/Scripts/PlayerLook.cs
+++ ProjectGuard/Assets/Scripts/PlayerLook.cs
@@ -1,38 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerLook : MonoBehaviour {
+public class PlayerLook : MonoBehaviour
+{
 	public GameObject camObj;
 	private Vector2 mousePos;
 	private float cameraDistance;
 	private Vector3 worldPos;
+	private Vector3 lockedLookDirection;
 	public float rotationSpeed;
 	
-	private bool playerCanRotate;
+	private bool m_playerCanRotate;
 
-	void Start () {
+	void Start ()
+	{
 		camObj = GameObject.FindGameObjectWithTag ("MainCamera");
 		playerCanRotate = true;
 	}
 
-	void FixedUpdate () {
-		//convert mouseposition to relatives from 0 to 1
-		mousePos = new Vector2 (Mathf.InverseLerp (0, Screen.width, Input.mousePosition.x), Mathf.InverseLerp (0, Screen.height, Input.mousePosition.y));
-
+	void FixedUpdate ()
+	{
+		RotatePlayer ();
+	}
+	
+	private void RotatePlayer ()
+	{
 		if (playerCanRotate) {
+			
+			//convert mouseposition to relatives from 0 to 1
+			mousePos = new Vector2 (Mathf.InverseLerp (0, Screen.width, Input.mousePosition.x), Mathf.InverseLerp (0, Screen.height, Input.mousePosition.y));
+			
 			//raycast trough viewspace and into the world
 			Ray mousePoint = camObj.GetComponent<Camera> ().ViewportPointToRay (new Vector3 (mousePos.x, mousePos.y, 0));
 			RaycastHit hit;
 			Physics.Raycast (mousePoint, out hit);
 			worldPos = hit.point;
+			lockedLookDirection = worldPos - transform.position;
 		}
+		
 		//Lerp towards decired rotation
-		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (worldPos - transform.position), Time.deltaTime * rotationSpeed);
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (lockedLookDirection), Time.deltaTime * rotationSpeed);
 		//Freeze x and z rotation
 		transform.rotation = Quaternion.Euler (new Vector3 (0, transform.rotation.eulerAngles.y, 0));
 	}
 	
-	public void SetPlayerCanRotate (bool value) {
-		playerCanRotate = value;
+	public bool playerCanRotate {
+		get { return m_playerCanRotate;}
+		set { m_playerCanRotate = value;}
+	}
+	
+	public void LockPlayerOnDirection (Vector3 _direction)
+	{
+		playerCanRotate = false;
+		lockedLookDirection = _direction.normalized;
 	}
 }
