6c43ec60b2a1b745e5eefcd62507ab3e4dfd1368
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-03-14 14:22:35 +0100

    DodgeRotation

--- ProjectGuard/Assets/Scripts/AnimationScripts.cs
+++ ProjectGuard/Assets/Scripts/AnimationScripts.cs
@@ -4,82 +4,82 @@ using System.Collections;
 public class AnimationScripts : MonoBehaviour
 {
 
-		private GameObject player;
-		public bool isAttacking;
+	private GameObject player;
+	public bool isAttacking;
 
-		// Use this for initialization
-		void Start ()
-		{
-				player = GameObject.Find ("Player");
-		}
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Player");
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
+	// Update is called once per frame
+	void Update ()
+	{
 	
-		}
+	}
 
-		//called form animations
-		void IniAttack ()
-		{
-				isAttacking = true;
-				//clear hits
-				player.GetComponent<AttackController> ().targetsHit.Clear ();
-				//don't attack again
-				GetComponent<Animator> ().SetBool ("Attack", false);
-				//can't attack
-				GetComponent<Animator> ().SetBool ("CanAttack", false);
-				//can't move or rotate
-				player.GetComponent<MovementController> ().SetCanMove (false);
-				player.GetComponent<PlayerLook> ().SetPlayerCanRotate (false);
-				//no damage
-				player.GetComponent<AttackController> ().doesDamage = false;
+	//called form animations
+	void IniAttack ()
+	{
+		isAttacking = true;
+		//clear hits
+		player.GetComponent<AttackController> ().targetsHit.Clear ();
+		//don't attack again
+		GetComponent<Animator> ().SetBool ("Attack", false);
+		//can't attack
+		GetComponent<Animator> ().SetBool ("CanAttack", false);
+		//can't move or rotate
+		player.GetComponent<MovementController> ().SetCanMove (false);
+		player.GetComponent<PlayerLook> ().playerCanRotate = false;
+		//no damage
+		player.GetComponent<AttackController> ().doesDamage = false;
+	}
+	void IniRebound ()
+	{
+		isAttacking = false;
+		//don't rebound again
+		GetComponent<Animator> ().SetBool ("Rebound", false);
+		//don't attack again
+		GetComponent<Animator> ().SetBool ("Attack", false);
+		//can't attack
+		GetComponent<Animator> ().SetBool ("CanAttack", false);
+		//can't move or rotate
+		player.GetComponent<MovementController> ().SetCanMove (false);
+		player.GetComponent<PlayerLook> ().playerCanRotate = false;
+		//no damage
+		player.GetComponent<AttackController> ().doesDamage = false;
+	}
+	void CanAttack ()
+	{
+		GetComponent<Animator> ().SetBool ("CanAttack", true);
+		isAttacking = false;
+		player.GetComponent<AttackController> ().inAnAttack = false;
+	}
+	void CanNotAttack ()
+	{
+		GetComponent<Animator> ().SetBool ("CanAttack", false);
+	}
+	void Damage ()
+	{
+		player.GetComponent<AttackController> ().doesDamage = true;
+	}
+	void NoDamage ()
+	{
+		player.GetComponent<AttackController> ().doesDamage = false;
+	}
+	void CanMove ()
+	{
+		if (!isAttacking) {
+			player.GetComponent<MovementController> ().SetCanMove (true);
 		}
-		void IniRebound ()
-		{
-				isAttacking = false;
-				//don't rebound again
-				GetComponent<Animator> ().SetBool ("Rebound", false);
-				//don't attack again
-				GetComponent<Animator> ().SetBool ("Attack", false);
-				//can't attack
-				GetComponent<Animator> ().SetBool ("CanAttack", false);
-				//can't move or rotate
-				player.GetComponent<MovementController> ().SetCanMove (false);
-				player.GetComponent<PlayerLook> ().SetPlayerCanRotate (false);
-				//no damage
-				player.GetComponent<AttackController> ().doesDamage = false;
-		}
-		void CanAttack ()
-		{
-				GetComponent<Animator> ().SetBool ("CanAttack", true);
-				isAttacking = false;
-				player.GetComponent<AttackController> ().inAnAttack = false;
-		}
-		void CanNotAttack ()
-		{
-				GetComponent<Animator> ().SetBool ("CanAttack", false);
-		}
-		void Damage ()
-		{
-				player.GetComponent<AttackController> ().doesDamage = true;
-		}
-		void NoDamage ()
-		{
-				player.GetComponent<AttackController> ().doesDamage = false;
-		}
-		void CanMove ()
-		{
-				if (!isAttacking) {
-						player.GetComponent<MovementController> ().SetCanMove (true);
-				}
-		}
-		void CanNotMove ()
-		{
-				player.GetComponent<MovementController> ().SetCanMove (false);
-		}
-		void CanRotate ()
-		{
-				player.GetComponent<PlayerLook> ().SetPlayerCanRotate (true);
-		}
+	}
+	void CanNotMove ()
+	{
+		player.GetComponent<MovementController> ().SetCanMove (false);
+	}
+	void CanRotate ()
+	{
+		player.GetComponent<PlayerLook> ().playerCanRotate = true;
+	}
 }
--- ProjectGuard/Assets/Scripts/Items/Item.cs
+++ ProjectGuard/Assets/Scripts/Items/Item.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class Item : MonoBehaviour
 {
-		public float maxPickupRange;
-		protected Transform holdPoint;
-		protected bool m_equipped = false;
+	public float maxPickupRange;
+	protected Transform holdPoint;
+	protected bool m_equipped = false;
 		
-		public bool equipped {
-				get { return m_equipped;}
-				set { 
-						m_equipped = value;
-						this.collider.enabled = !value;
-				}
+	public bool equipped {
+		get { return m_equipped;}
+		set { 
+			m_equipped = value;
+			this.collider.enabled = !value;
 		}
+	}
 }
