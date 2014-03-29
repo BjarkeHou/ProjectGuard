d32b89d6e5d8fd0ea349de6c9a82917b62fc3139
Ensutee <magnus_s_n@hotmail.com>
2014-03-29 21:34:02 +0100

    Implemented a post image effect to create a ghost world. Can be accessed through the main camera and activated through the transition variable (0 -> 1)

---
+++ ProjectGuard/Assets/Scripts/Shader/GhostWorld.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostWorld : MonoBehaviour {
+
+	public Material ghostWorld;
+	public float transition;
+	public float blueshift;
+	public float waveCount;
+	public float waveSize;
+
+	// Use this for initialization
+	void Start() {
+		ghostWorld.SetFloat("_Transition", Mathf.Clamp(transition, 0, 1));
+		ghostWorld.SetFloat("_BlueShift", blueshift);
+		ghostWorld.SetFloat("_WaveCount", waveCount);
+		ghostWorld.SetFloat("_WaveSize", waveSize);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		ghostWorld.SetFloat("_Transition", Mathf.Clamp(transition, 0, 1));
+		ghostWorld.SetFloat("_BlueShift", blueshift);
+		ghostWorld.SetFloat("_WaveCount", waveCount);
+		ghostWorld.SetFloat("_WaveSize", waveSize);
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		Graphics.Blit(src, dest, ghostWorld);
+	}
+}
