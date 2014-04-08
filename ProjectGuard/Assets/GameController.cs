4122a680acba75be733dfd975d88b28c99caf2d9
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-04-08 12:14:17 +0200

    MainMenu added, working on pause menu..

---
+++ ProjectGuard/Assets/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	private bool m_isPaused = false;
+	
+	public bool isPaused
+	{
+		get { return m_isPaused; }
+		set { m_isPaused = value; }
+	}
+
+	public void PauseGame()
+	{
+		isPaused = true;
+		Time.timeScale = 0;
+	}
+	
+	public void UnPauseGame()
+	{
+		isPaused = false;
+		Time.timeScale = 1;
+	}
+}
--- ProjectGuard/Assets/Scripts/Controllers/InputController.cs
+++ ProjectGuard/Assets/Scripts/Controllers/InputController.cs
@@ -9,39 +9,59 @@ public class InputController : MonoBehaviour
 	private MovementController mCtrl;
 	private AttackController aCtrl;
 	private EquipmentController eCtrl;
+	private GameController gCtrl;
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-		GameObject p = GameObject.FindGameObjectWithTag ("Player");
-		mCtrl = p.GetComponent<MovementController> ();
-		aCtrl = p.GetComponent<AttackController> ();
-		eCtrl = p.GetComponent<EquipmentController> ();
+		GameObject p = GameObject.FindGameObjectWithTag("Player");
+		mCtrl = p.GetComponent<MovementController>();
+		aCtrl = p.GetComponent<AttackController>();
+		eCtrl = p.GetComponent<EquipmentController>();
+		gCtrl = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
+		// Pause the game
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			if (gCtrl.isPaused)
+			{
+				gCtrl.UnPauseGame();
+			} else if (!gCtrl.isPaused)
+			{
+				gCtrl.PauseGame();
+			}
+		}
+	
 		// Move Player
-		v = Input.GetAxisRaw ("Vertical");
-		h = Input.GetAxisRaw ("Horizontal");
-		mCtrl.MoveCharacter (v, h);
-		if (Input.GetKeyDown (KeyCode.LeftShift)) {
-			mCtrl.Dodge (v, h);
+		v = Input.GetAxisRaw("Vertical");
+		h = Input.GetAxisRaw("Horizontal");
+		mCtrl.MoveCharacter(v, h);
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			mCtrl.Dodge(v, h);
 		}
 				
 		// Perform attack
-		if (Input.GetMouseButtonDown (0) && eCtrl.hasWeaponEquipped) {
-			aCtrl.DeclareAttack ();
+		if (Input.GetMouseButtonDown(0) && eCtrl.hasWeaponEquipped)
+		{
+			aCtrl.DeclareAttack();
 		}
 
-		if (Input.GetMouseButtonDown (1) && eCtrl.hasWeaponEquipped) {
-			aCtrl.DeclareParry ();
+		if (Input.GetMouseButtonDown(1) && eCtrl.hasWeaponEquipped)
+		{
+			aCtrl.DeclareParry();
 		}
 
 		// Drop weapon
-		if (Input.GetKey (KeyCode.G)) {
-			eCtrl.Drop (ItemType.Weapon);
+		if (Input.GetKey(KeyCode.G))
+		{
+			eCtrl.Drop(ItemType.Weapon);
 		}
+		
+
 	}
 }
