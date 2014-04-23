bdeab39fb5b77344875ba3ff4e8982685b6c1b08
Ensutee <magnus_s_n@hotmail.com>
2014-04-23 02:07:06 +0200

    Refined the trap system and implemented a propper model for the spear trap (refined hinge trap)

---
+++ ProjectGuard/Assets/Scripts/Traps/SpearBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpearBehaviour : MonoBehaviour {
+	
+	private TrapDeadlyPart deadlyPart;
+	private bool isTriggered;
+	public float triggerDelay;
+	private float timer;
+
+	public bool isRepeatable;
+	private bool canTrigger;
+
+	void Start() {
+		deadlyPart = GetComponentInChildren<TrapDeadlyPart>();
+
+		timer = 0;
+		canTrigger = true;
+	}
+
+	void Update() {
+		if (isTriggered) {
+			canTrigger = false;
+			Behave();
+		}
+	}
+
+	public void Trigger () {
+		if (canTrigger) {
+			isTriggered = true;
+		}
+	}
+
+	void Behave() {
+		if (timer == 0) {
+			timer = Time.time;
+		}
+
+		if (Time.time > timer + triggerDelay) {
+			GetComponent<Animator>().SetBool("Trigger", true);
+		}
+	}
+
+	void Reset() {
+		GetComponent<Animator>().SetBool("Trigger", false);
+		isTriggered = false;
+		timer = 0;
+
+		if (isRepeatable) {
+			deadlyPart.targetsHit.Clear();
+			canTrigger = true;
+		}
+	}
+}
