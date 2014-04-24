26f9a2a6fc6ce8368a5edf547433a6721908a3d6
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-04-24 14:48:16 +0200

    start på dialog screen

--- ProjectGuard/Assets/Scripts/Controllers/DialogController.cs
+++ ProjectGuard/Assets/Scripts/Controllers/DialogController.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class DialogController : MonoBehaviour
 {
 	public GUISkin gui_Skin;
+	public float dialogRange;
 
 	private GameController game;
+	private GameObject player;
 	
 	private bool showDialogLabel;
 
@@ -14,6 +16,7 @@ public class DialogController : MonoBehaviour
 	{
 		showDialogLabel = false;
 		game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	void OnMouseEnter()
@@ -32,12 +35,30 @@ public class DialogController : MonoBehaviour
 		}
 	}
 	
+	void OnMouseDown()
+	{
+		if (game.isInGhostMode && !game.isInDialogMode && !game.isPaused && showDialogLabel && dialogRange > Vector3.Distance(this.transform.position, player.transform.position))
+		{
+			game.isInDialogMode = true;
+			// Lerp camera here....
+			
+			
+		}
+	}
+	
 	void OnGUI()
 	{
-		if (game.isInGhostMode && !game.isPaused && showDialogLabel)
+		GUI.skin = gui_Skin;
+		
+		// Vis mouseover på at ham kan man snakke med
+		if (game.isInGhostMode && !game.isPaused && showDialogLabel && !game.isInDialogMode)
 		{
-			GUI.skin = gui_Skin;
 			GUI.Label(new Rect(Input.mousePosition.x + 20, Screen.height - Input.mousePosition.y, 300, 200), "Click to speak with " + this.gameObject.name);
+		}
+		
+		if (game.isInDialogMode && game.isInGhostMode && !game.isPaused)
+		{
+			
 		}
 	}
 }
--- ProjectGuard/Assets/Scripts/Controllers/GameController.cs
+++ ProjectGuard/Assets/Scripts/Controllers/GameController.cs
@@ -7,13 +7,20 @@ public class GameController : MonoBehaviour
 	public float timeToReviveInGhostMode;
 	
 	private bool m_isPaused = false;
-	public bool m_isInGhostMode = false;
+	private bool m_isInGhostMode = false;
+	private bool m_isInDialogMode = false;
 	private float m_timeLeftToReviveFromGhostMode;
 	
 	public bool isPaused
 	{
 		get { return m_isPaused; }
 		set { m_isPaused = value; }
+	}
+	
+	public bool isInDialogMode
+	{
+		get { return m_isInDialogMode; }
+		set { m_isInDialogMode = value; }
 	}
 	
 	public bool isInGhostMode
