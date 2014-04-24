6290c0ae17fed6e0bbaf232128045d658a8cea77
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-04-25 00:00:56 +0200

    Menu og Interface

--- ProjectGuard/Assets/Scenes/MainMenu/Scripts/MainMenu.cs
+++ ProjectGuard/Assets/Scenes/MainMenu/Scripts/MainMenu.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+	public GUIStyle style;
+
 	public GUISkin guiSkin;
 	public Texture background;
 	public Texture logo;
+	
+	public Texture[] playButton;
+	public Texture[] settingsButton;
+	public Texture[] exitButton;
 	
 	public float logoX;
 	public float logoY;
@@ -31,20 +37,20 @@ public class MainMenu : MonoBehaviour
 		
 		GUI.DrawTexture(new Rect(Screen.width * logoX * 0.01f, Screen.height * logoY * 0.01f, Screen.width * logoWidth * 0.01f, Screen.height * logoHeight * 0.01f), logo);
 		
-		if (GUI.Button(new Rect(Screen.width * playButtonX * 0.01f, Screen.height * playButtonY * 0.01f, Screen.width * buttonWidth * 0.01f, Screen.height * buttonHeight * 0.01f), "Play Game"))
+		if (GUI.Button(new Rect(Screen.width * playButtonX * 0.01f, Screen.height * playButtonY * 0.01f, Screen.width * buttonWidth * 0.01f, Screen.height * buttonHeight * 0.01f), "PLAY"))
 		{
 			print("Play Game!");
 			Application.LoadLevel(1);
 		}
 		
-		if (GUI.Button(new Rect(Screen.width * settingsButtonX * 0.01f, Screen.height * settingsButtonY * 0.01f, Screen.width * buttonWidth * 0.01f, Screen.height * buttonHeight * 0.01f), "Settings"))
+		if (GUI.Button(new Rect(Screen.width * settingsButtonX * 0.01f, Screen.height * settingsButtonY * 0.01f, Screen.width * buttonWidth * 0.01f, Screen.height * buttonHeight * 0.01f), "SETTINGS"))
 		{
 			print("Settings!");
 		}
 		
-		if (GUI.Button(new Rect(Screen.width * tutorialButtonX * 0.01f, Screen.height * tutorialButtonY * 0.01f, Screen.width * buttonWidth * 0.01f, Screen.height * buttonHeight * 0.01f), "Tutorial"))
+		if (GUI.Button(new Rect(Screen.width * tutorialButtonX * 0.01f, Screen.height * tutorialButtonY * 0.01f, Screen.width * buttonWidth * 0.01f, Screen.height * buttonHeight * 0.01f), "EXIT"))
 		{
-			print("Tutorial!");
+			Application.Quit();
 		}
 	}
 }
