6a8e6a7b20dd2ba2938be80346fa0659e0b752ea
Ensutee <magnus_s_n@hotmail.com>
2014-04-24 05:15:29 +0200

    Current will now functions correctly as effective health and will kill you.

--- ProjectGuard/Assets/Scripts/Interface.cs
+++ ProjectGuard/Assets/Scripts/Interface.cs
@@ -5,29 +5,38 @@ public class Interface : MonoBehaviour
 {
 
 	private PlayerWillController pWill;
+	private HealthController hCtrl;
 	private GameController game;
 
 	public GUISkin guiSkin;
 
 	private Texture2D barTex;
 	private Texture2D spentTex;
+	private Texture2D damageTex;
 	private Texture2D bgTex;
 	private Texture2D borderTex;
 	public Color barCol;
 	public Color spentCol;
+	public Color damageCol;
 	public Color bgCol;
 	public Color borderCol;
-	
-	
+
 	public float PauseMenuButtonHeight;
+
+	private int prevHealth;
+	private int damageTaken;
+	private float barAlpha;
 
 	// Use this for initialization
 	void Start()
 	{
 		pWill = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerWillController>();
+		hCtrl = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthController>();
 		game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
 
 		SetupWillBar();
+
+		prevHealth = hCtrl.getCurrentHealth();
 	}
 
 	void OnGUI()
@@ -77,6 +86,10 @@ public class Interface : MonoBehaviour
 		spentTex = new Texture2D(1, 1);
 		spentTex.SetPixel(0, 0, spentCol);
 		spentTex.Apply();
+
+		damageTex = new Texture2D(1, 1);
+		damageTex.SetPixel(0, 0, damageCol);
+		damageTex.Apply();
 		
 		bgTex = new Texture2D(1, 1);
 		bgTex.SetPixel(0, 0, bgCol);
@@ -97,12 +110,36 @@ public class Interface : MonoBehaviour
 		
 		//Spent
 		float width = dimension.x * Mathf.InverseLerp(0, pWill.MaxWill, pWill.CurMaxWill);
-		
 		GUI.DrawTexture(new Rect(anchor.x, anchor.y, width, dimension.y), spentTex);
 		
 		//Bar
 		width = dimension.x * Mathf.InverseLerp(0, pWill.MaxWill, pWill.CurWill);
 		GUI.DrawTexture(new Rect(anchor.x, anchor.y, width, dimension.y), barTex);
+
+		//Damage taken
+		print(prevHealth);
+		if (hCtrl.getCurrentHealth() != prevHealth) {
+			if (hCtrl.getCurrentHealth() < prevHealth) {
+				damageTaken = hCtrl.LastDamageTaken;
+				barAlpha = 1;
+			}
+			prevHealth = hCtrl.getCurrentHealth();
+		}
+
+		if (barAlpha > 0) {
+
+			//set color (alpha)
+			Color guiCol = GUI.color;
+			GUI.color = new Color(1, 1, 1, barAlpha * 0.75f);
+
+			width = dimension.x * Mathf.InverseLerp(0, pWill.MaxWill, Mathf.Abs(hCtrl.LastDamageTaken));
+			GUI.DrawTexture(new Rect(anchor.x, anchor.y, width, dimension.y), damageTex);
+
+			//reduce alpha
+			barAlpha -= Time.deltaTime;
+
+			GUI.color = guiCol;
+		}
 		
 		//Border
 		GUI.DrawTexture(new Rect(anchor.x, anchor.y, dimension.x, 1), borderTex);
