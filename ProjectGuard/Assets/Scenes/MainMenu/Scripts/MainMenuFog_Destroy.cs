0c4e30c57f94f1ad2797d268ffa3dee10e75d130
Ensutee <magnus_s_n@hotmail.com>
2014-05-13 16:23:06 +0200

    New main menu with fog!

---
+++ ProjectGuard/Assets/Scenes/MainMenu/Scripts/MainMenuFog_Destroy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFog_Destroy : MonoBehaviour {
+
+	public bool goLeft;
+	public float speed;
+
+	// Use this for initialization
+	void Start() {
+		if (!goLeft) {
+			speed *= -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		transform.position = new Vector3 (transform.position.x + (speed * Time.deltaTime), transform.position.y, transform.position.z);
+
+		if (transform.localPosition.x < -2.5f) {
+			Destroy(gameObject);
+		} else if (transform.localPosition.x > 2.5f) {
+			Destroy(gameObject);
+		}
+	}
+}
