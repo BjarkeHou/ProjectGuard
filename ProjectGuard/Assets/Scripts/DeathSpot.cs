fe57b50ea7dd8f8728e74ac1358028d3759e0f3a
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-05-13 20:53:05 +0200

    Alle enemies er hævet.. Weapon label er fjernet Du resser nu hurtigt..

--- ProjectGuard/Assets/Scripts/DeathSpot.cs
+++ ProjectGuard/Assets/Scripts/DeathSpot.cs
@@ -4,43 +4,49 @@ using System.Collections;
 public class DeathSpot : MonoBehaviour
 {
 
-		private GameController game;
-		private HealthController hCtrl;
-		public float reviveSpeed;
-		private bool spawn;
+	private GameController game;
+	private HealthController hCtrl;
+	public float reviveSpeed;
+	private bool spawn;
 
-		// Use this for initialization
-		void Start ()
+	// Use this for initialization
+	void Start()
+	{
+		game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		GameObject.Find("GUI").GetComponent<Interface>().RezzSpot = gameObject;
+	}
+
+	void Update()
+	{
+		if (!game.isInGhostMode)
 		{
-				game = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
-				GameObject.Find ("GUI").GetComponent<Interface> ().RezzSpot = gameObject;
+			Destroy(gameObject);
 		}
 
-		void Update ()
+		if (spawn && game.isInGhostMode)
 		{
-				if (!game.isInGhostMode) {
-						Destroy (gameObject);
-				}
-
-				if (spawn && game.isInGhostMode) {
-						hCtrl = GameObject.FindWithTag ("Player").GetComponent<HealthController> ();
-						if (game.timeLeftToReviveFromGhostMode <= game.timeToReviveInGhostMode) {
-								GameObject.FindWithTag ("Player").GetComponent<MovementController> ().SetCanMove (false);
-								game.timeLeftToReviveFromGhostMode += reviveSpeed * Time.deltaTime;
-						} else {
-								hCtrl.adjustCurrentHealth (hCtrl.getMaxHealth () - hCtrl.getCurrentHealth ());
-								GameObject.FindWithTag ("Player").GetComponent<MovementController> ().SetCanMove (true);
-						}
-				} else if (game.isInGhostMode) {
-						spawn = false;
-				}
-		}
-
-		void OnTriggerStay (Collider other)
+			hCtrl = GameObject.FindWithTag("Player").GetComponent<HealthController>();
+			if (game.timeLeftToReviveFromGhostMode <= game.timeToReviveInGhostMode)
+			{
+				GameObject.FindWithTag("Player").GetComponent<MovementController>().SetCanMove(false);
+				game.timeLeftToReviveFromGhostMode += reviveSpeed * Time.deltaTime;
+			} else
+			{
+				hCtrl.adjustCurrentHealth(hCtrl.getMaxHealth() - hCtrl.getCurrentHealth());
+				GameObject.FindWithTag("Player").GetComponent<MovementController>().SetCanMove(true);
+			}
+		} else if (game.isInGhostMode)
 		{
-				if (Input.GetKeyDown (KeyCode.R)) {
-						spawn = true;
-
-				}
+			spawn = false;
 		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			spawn = true;
+
+		}
+	}
 }
--- ProjectGuard/Assets/Scripts/Items/Weapon.cs
+++ ProjectGuard/Assets/Scripts/Items/Weapon.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class Weapon : Item {
+public class Weapon : Item
+{
 	private AttackController atkCtrl;
 	private GameController game;
 
@@ -26,11 +27,14 @@ public class Weapon : Item {
 	public Shader Outlined { get { return outlined; } }
 
 	// Use this for initialization
-	void Start() {
+	void Start()
+	{
 		game = GameObject.FindWithTag("GameController").GetComponent<GameController>();
 
-		if (transform.parent != null) {
-			if (transform.parent.tag == "Player" || transform.parent.tag == "Enemy") {
+		if (transform.parent != null)
+		{
+			if (transform.parent.tag == "Player" || transform.parent.tag == "Enemy")
+			{
 				TryToEquip(transform.parent.gameObject);
 			}
 		}
@@ -42,15 +46,19 @@ public class Weapon : Item {
 		outlined = Shader.Find("Toon/Basic Outline");
 	}
 	
-	void OnGUI() {
-		if (showStatsLabel && !equipped) {
-			GUI.skin = gui_Skin;
-			GUI.Label(new Rect (Input.mousePosition.x + 20, Screen.height - Input.mousePosition.y, 200, 200), "Name: " + weaponName + "\nAge: " + age + "\nDamage: " + damage);
+	void OnGUI()
+	{
+		if (showStatsLabel && !equipped)
+		{
+//			GUI.skin = gui_Skin;
+//			GUI.Label(new Rect (Input.mousePosition.x + 20, Screen.height - Input.mousePosition.y, 200, 200), "Name: " + weaponName + "\nAge: " + age + "\nDamage: " + damage);
 		}
 	}
 	
-	public void TryToEquip(GameObject wielder) {
-		if (!equipped) {
+	public void TryToEquip(GameObject wielder)
+	{
+		if (!equipped)
+		{
 			transform.Find("PickUpBox").collider.enabled = false;
 			equipped = true; 
 			atkCtrl = wielder.GetComponent<AttackController>();
@@ -58,64 +66,76 @@ public class Weapon : Item {
 		}
 	}
 	
-	public void Dropped() {
-		if (equipped) {
+	public void Dropped()
+	{
+		if (equipped)
+		{
 			transform.Find("PickUpBox").collider.enabled = true;
 			equipped = false;
 			this.transform.parent = null;
 		}
 	}
 	
-	void LateUpdate() {
+	void LateUpdate()
+	{
 		//Update position and check for collisions
-		if (equipped) {
+		if (equipped)
+		{
 			CheckHit();
 		}
 	}
 
-	void CheckHit() {
+	void CheckHit()
+	{
 		RaycastHit hit;
-		Ray charles = new Ray (holdPoint.position, tipPoint.position - holdPoint.position);
+		Ray charles = new Ray(holdPoint.position, tipPoint.position - holdPoint.position);
 		float dist = (tipPoint.position - holdPoint.position).magnitude;
 		
 		if (Physics.Raycast(charles, out hit, dist) &&
 			atkCtrl.doesDamage &&
 			game.GetComponent<GhostWorldController>().deathTransition <= 0 &&
 			hit.collider.gameObject.layer != LayerMask.NameToLayer("DeadEnemies") &&
-			hit.collider.gameObject.layer != LayerMask.NameToLayer("WeaponPickUp")) {
+			hit.collider.gameObject.layer != LayerMask.NameToLayer("WeaponPickUp"))
+		{
 
 			GameObject obj = hit.collider.gameObject;
 			//if an enemy is hit
-			if ((obj.tag == "Enemy" || obj.tag == "Player")) {
+			if ((obj.tag == "Enemy" || obj.tag == "Player"))
+			{
 				int hitType = atkCtrl.Hit(obj, damage);
-				if (hitType == 1) {
+				if (hitType == 1)
+				{
 					//Instantiate blood
 					GameObject blood = (GameObject)Instantiate(Resources.Load("Prefabs/Blood"));
 					blood.transform.parent = transform;
 					blood.GetComponent<BloodDestroy>().origin = hit.point;
 					
 					print("HIT on " + obj.name);
-				} else if (hitType == 0) {
+				} else if (hitType == 0)
+				{
 					//Instantiate sparks
 					GameObject spark = (GameObject)Instantiate(Resources.Load("Prefabs/Sparks"));
 					spark.transform.position = hit.point;
 					spark.transform.rotation = Quaternion.LookRotation(hit.normal);
 					
 					print("PARRY by " + obj.name);
-				} else if (hitType == 2) {
+				} else if (hitType == 2)
+				{
 					//in case of friendly fire
 					print("Friendly Fire");
 				}
 				
 				//if an object is hit
-			} else {
+			} else
+			{
 				//Instantiate sparks
 				GameObject spark = (GameObject)Instantiate(Resources.Load("Prefabs/Sparks"));
 				spark.transform.position = hit.point;
 				spark.transform.rotation = Quaternion.LookRotation(hit.normal);
 				
 				//If collision is within skin depth
-				if ((hit.point - holdPoint.position).magnitude < (skinPoint.position - holdPoint.position).magnitude) {
+				if ((hit.point - holdPoint.position).magnitude < (skinPoint.position - holdPoint.position).magnitude)
+				{
 					print("REBOUND on " + obj.name);
 					atkCtrl.Rebound();
 				}
@@ -125,31 +145,38 @@ public class Weapon : Item {
 
 	//HACK
 	//in case hand hits before blade
-	void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter(Collider other)
+	{
 		GameObject obj = other.gameObject;
 		//if an enemy is hit
-		if (equipped && atkCtrl.doesDamage && !game.isInGhostMode && obj.layer != LayerMask.NameToLayer("DeadEnemies")) {
-			if ((obj.tag == "Enemy" || obj.tag == "Player")) {
+		if (equipped && atkCtrl.doesDamage && !game.isInGhostMode && obj.layer != LayerMask.NameToLayer("DeadEnemies"))
+		{
+			if ((obj.tag == "Enemy" || obj.tag == "Player"))
+			{
 				int hitType = atkCtrl.Hit(obj, damage);
-				if (hitType == 1) {
+				if (hitType == 1)
+				{
 					//Instantiate blood
 					GameObject blood = (GameObject)Instantiate(Resources.Load("Prefabs/Blood"));
 					blood.GetComponent<BloodDestroy>().origin = transform.position;
 					blood.transform.parent = transform;
 					
 					print("HIT on " + obj.name);
-				} else if (hitType == 0) {
+				} else if (hitType == 0)
+				{
 					print("PARRY by " + obj.name);
 					//Instantiate sparks
 					GameObject spark = (GameObject)Instantiate(Resources.Load("Prefabs/Sparks"));
 					spark.transform.position = transform.position;
-				} else if (hitType == 2) {
+				} else if (hitType == 2)
+				{
 					//print("Friendly Fire");
 				}
 				
 				//if an object is hit
 
-			} else {
+			} else
+			{
 				//Instantiate sparks
 				GameObject spark = (GameObject)Instantiate(Resources.Load("Prefabs/Sparks"));
 				spark.transform.position = transform.position;
